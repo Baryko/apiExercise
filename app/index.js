e8c895c8b20ab1e6918d1b3a377ced6dd842c7da
Bartosz Korolik <korolik.bartosz@gmail.com>
2022-10-20 16:36:50 +0200

    add sort logic to index.js

--- app/index.js
+++ app/index.js
@@ -10,8 +10,15 @@ const err = document.querySelector('.error');
 const textInput = document.querySelector('.textInput');
 const allAElements = [...document.querySelectorAll('a')];
 const allIElementsInPagination = [...document.querySelector(".paginationContainer").querySelectorAll('i')];
+const allFirstColumnTds = [...document.querySelectorAll('td:nth-child(1)')];
+const allSecondColumnTds = [...document.querySelectorAll('td:nth-child(2)')];
+const allThirdColumnTds = [...document.querySelectorAll('td:nth-child(3)')];
+const allFourthColumnTds = [...document.querySelectorAll('td:nth-child(4)')];
+const sortElement = document.querySelector('#sort');
 
 let page = 1;
+let sort = "";
+let order = "";
 
 
 const showError = () => {
@@ -59,61 +66,7 @@ const addBlurToArrows = (page) => {
     if (page === allAElements.length - 2) {
         allIElementsInPagination[1].style.opacity = "50%"
     } else allIElementsInPagination[1].style.opacity = "100%"
-}
-
-
-const getData = (e) => {
-    e.preventDefault();
-    hideTable();
-    hideError();
-    hideBasicMessage();
-    showSpinner();
-    addBlurToArrows(page);
-    const value = getInputValue();
-
-    const options = {
-        headers: {
-            Authorization: 'Bearer ' + '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',
-            Accept: 'application/vnd.github+json'
-        }
-    };
-
-    const url = `https://api.github.com/search/repositories?q=${value}&page=${page}&per_page=10`;
-
-    axios.get(url, options)
-        .then((res) => {
-
-            handleData(res.data.items);
-            showTable();
-            hideSpinner();
-            hideBasicMessage();
-        },)
-        .catch((error) => {
-            hideTable();
-            hideSpinner();
-            showError();
-            handleData(error);
-            hideBasicMessage()
-
-        })
 };
-
-const modifyButton = (e) => {
-    if (e.target.value.length > 0) {
-        submitInput.disabled = false;
-    } else submitInput.disabled = true;
-};
-
-
-
-submitInput.addEventListener('click', (e) => getData(e));
-textInput.addEventListener('keyup', (e) => modifyButton(e));
-
-const allFirstColumnTds = [...document.querySelectorAll('td:nth-child(1)')];
-const allSecondColumnTds = [...document.querySelectorAll('td:nth-child(2)')];
-const allThirdColumnTds = [...document.querySelectorAll('td:nth-child(3)')];
-const allFourthColumnTds = [...document.querySelectorAll('td:nth-child(4)')];
-
 
 const handleData = function (data) {
     allFirstColumnTds.forEach((td, index) => td.textContent = data[index].name)
@@ -123,6 +76,12 @@ const handleData = function (data) {
         td.textContent = converYearFromJson(data[index].created_at)
     })
 }
+
+const modifyButtonAvailiability = (e) => {
+    if (e.target.value.length > 0) {
+        submitInput.disabled = false;
+    } else submitInput.disabled = true;
+};
 
 
 const converYearFromJson = (isoData) => {
@@ -166,6 +125,55 @@ const movePage = (e, index) => {
     allAElements[page].classList.add('active');
 }
 
+const sortTable = (e, i) => {
+    const optionIndex = e.target.options.selectedIndex
+    const optionName = e.target.options[optionIndex].getAttribute("name");
+    const optionValue = e.target.value;
+    sort = optionName;
+    order = optionValue;
+    getData(e);
+}
+
+const getData = (e) => {
+    e.preventDefault();
+    hideTable();
+    hideError();
+    hideBasicMessage();
+    showSpinner();
+    addBlurToArrows(page);
+    const value = getInputValue();
+
+    const options = {
+        headers: {
+            Authorization: 'Bearer ' + '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',
+            Accept: 'application/vnd.github+json'
+        }
+    };
+
+    const url = `https://api.github.com/search/repositories?q=${value}&page=${page}&per_page=10&sort=${sort}&order=${order}`;
+
+    axios.get(url, options)
+        .then((res) => {
+
+            handleData(res.data.items);
+            showTable();
+            hideSpinner();
+            hideBasicMessage();
+        },)
+        .catch((error) => {
+            hideTable();
+            hideSpinner();
+            showError();
+            handleData(error);
+            hideBasicMessage()
+
+        })
+};
+
+
+submitInput.addEventListener('click', (e) => getData(e));
+textInput.addEventListener('keyup', (e) => modifyButtonAvailiability(e));
+sortElement.addEventListener('change', (e) => sortTable(e));
 allAElements.forEach((a, i) => a.addEventListener('click', (e) => changePageOnClick(e, i)));
 allIElementsInPagination.forEach((i, index) => i.addEventListener('click', (e) => movePage(e, index)));
 
